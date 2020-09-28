d09c1e64bd2ef93d54cbc35f345df1f4f3dddf41
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-09-28 16:06:59 -0400

    added ses as an iterable, removed obsolete tasks

--- workflows/ExecuteTask.py
+++ workflows/ExecuteTask.py
@@ -26,6 +26,9 @@ if __name__ == '__main__':
     parser.add_argument('-c', required= True, type=str, default= argparse.SUPPRESS,
                         help='a single caseid or a .txt file where each line is a caseid')
 
+    parser.add_argument('-s', required= True, type=str, default= argparse.SUPPRESS,
+                        help='a single caseid or a .txt file where each line is a caseid')
+    
     parser.add_argument('--dwi-template', type=str, default='sub-$/dwi/*_dwi.nii.gz',
                         help='glob bids-data-dir/dwi-template to find input data')
 
@@ -52,199 +55,102 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     cases = read_cases(abspath(args.c)) if isfile(abspath(args.c)) else [args.c]
+    sessions = read_cases(abspath(args.s)) if isfile(abspath(args.s)) else [args.s]
+    
     args.bids_data_dir= abspath(args.bids_data_dir)
     derivatives_dir= pjoin('derivatives', args.derivatives_name)
     
     jobs = []
-    for id in cases:
-        # inter = IO(id, args.bids_data_dir, args.derivatives_dir)
+    for ses in sessions:
+        for id in cases:
+            # inter = IO(id, args.bids_data_dir, args.derivatives_dir)
 
-        if args.t2_template:
+            if args.t2_template:
 
-            if args.task=='StructMask':
-                jobs.append(StructMask(bids_data_dir=args.bids_data_dir,
-                                       derivatives_dir=derivatives_dir,
+                if args.task=='StructMask':
+                    jobs.append(StructMask(bids_data_dir=args.bids_data_dir,
+                                           derivatives_dir=derivatives_dir,
+                                           id=id,
+                                           struct_template=args.t2_template))
+
+                elif args.task=='Freesurfer':
+                    jobs.append(Freesurfer(bids_data_dir=args.bids_data_dir,
+                                           derivatives_dir=derivatives_dir, 
+                                           id=id,
+                                           t1_template=args.t1_template,
+                                           t2_template=args.t2_template))
+
+                elif args.task=='PnlEddyEpi' or args.task=='FslEddyEpi':
+                    jobs.append(eval(args.task)(bids_data_dir=args.bids_data_dir,
+                                                derivatives_dir=derivatives_dir,
+                                                id=id,
+                                                dwi_template=args.dwi_template,
+                                                struct_template=args.t2_template))
+
+
+                elif args.task=='Fs2Dwi':
+                    jobs.append(Fs2Dwi(bids_data_dir=args.bids_data_dir,
+                                                derivatives_dir=derivatives_dir,
+                                                id=id,
+                                                pa_ap_template=args.dwi_template,
+                                                t1_template=args.t1_template,
+                                                t2_template=args.t2_template))
+
+
+
+            # just t1_template
+            else:
+                if args.task=='StructMask':
+                    jobs.append(StructMask(bids_data_dir=args.bids_data_dir,
+                                           derivatives_dir=derivatives_dir,
+                                           id=id,
+                                           struct_template=args.t1_template))
+
+                elif args.task=='Freesurfer':
+                    jobs.append(Freesurfer(bids_data_dir=args.bids_data_dir,
+                                           derivatives_dir=derivatives_dir,
+                                           id=id,
+                                           t1_template=args.t1_template))
+
+                if args.task=='CnnMask':
+                    jobs.append(CnnMask(bids_data_dir=args.bids_data_dir,
+                                           derivatives_dir=derivatives_dir,
+                                           id=id,
+                                           ses=ses,
+                                           dwi_template=args.dwi_template))
+
+                elif args.task=='PnlEddy' or args.task=='FslEddy' or \
+                    args.task=='TopupEddy' or args.task=='CnnMaskPnlEddy':
+                    jobs.append(eval(args.task)(bids_data_dir=args.bids_data_dir,
+                                        derivatives_dir=derivatives_dir,
+                                        id=id,
+                                        pa_ap_template=args.dwi_template))
+
+
+                elif args.task=='PnlEddyUkf':
+                    jobs.append(PnlEddyUkf(bids_data_dir=args.bids_data_dir,
+                                           derivatives_dir=derivatives_dir,
+                                           id=id,
+                                           dwi_template=args.dwi_template))
+
+
+
+                elif args.task=='Wmqlqc':
+                    jobs.append(Wmqlqc(bids_data_dir=args.bids_data_dir,
                                        id=id,
-                                       struct_template=args.t2_template))
-
-            elif args.task=='Freesurfer':
-                jobs.append(Freesurfer(bids_data_dir=args.bids_data_dir,
-                                       derivatives_dir=derivatives_dir, 
-                                       id=id,
+                                       dwi_template=args.dwi_template,
+                                       dwi_align_prefix=inter['dwi_align_prefix'],
+                                       eddy_prefix=inter['eddy_prefix'],
+                                       eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
+                                       eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
                                        t1_template=args.t1_template,
-                                       t2_template=args.t2_template))
-
-            elif args.task=='PnlEddyEpi' or args.task=='FslEddyEpi':
-                jobs.append(eval(args.task)(bids_data_dir=args.bids_data_dir,
-                                            derivatives_dir=derivatives_dir,
-                                            id=id,
-                                            dwi_template=args.dwi_template,
-                                            struct_template=args.t2_template))
-
-            elif args.task=='Ukf':
-                jobs.append(Ukf(bids_data_dir = args.bids_data_dir,
-                                id=id,
-                                dwi_template = args.dwi_template,
-                                dwi_align_prefix=inter['dwi_align_prefix'],
-                                eddy_prefix=inter['eddy_prefix'],
-                                eddy_epi_prefix=inter['eddy_epi_prefix'],
-                                eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                eddy_epi_bse_masked_prefix=inter['eddy_epi_bse_masked_prefix'],
-                                eddy_epi_bse_betmask_prefix=inter['eddy_epi_bse_betmask_prefix'],
-                                struct_template= args.t2_template,
-                                struct_align_prefix=inter['t2_align_prefix'],
-                                mabs_mask_prefix=inter['t2_mabsmask_prefix'],
-                                tract_prefix= inter['eddy_epi_tract_prefix']))
-
-
-            elif args.task=='Fs2Dwi':
-                jobs.append(Fs2Dwi(bids_data_dir=args.bids_data_dir,
-                                            derivatives_dir=derivatives_dir,
-                                            id=id,
-                                            pa_ap_template=args.dwi_template,
-                                            t1_template=args.t1_template,
-                                            t2_template=args.t2_template))
-
-            elif args.task=='Wmql':
-                jobs.append(Wmql(bids_data_dir=args.bids_data_dir,
-                                 id=id,
-                                 dwi_template=args.dwi_template,
-                                 dwi_align_prefix=inter['dwi_align_prefix'],
-                                 eddy_prefix=inter['eddy_prefix'],
-                                 eddy_epi_prefix=inter['eddy_epi_prefix'],
-                                 eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                 eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                 eddy_epi_bse_masked_prefix=inter['eddy_epi_bse_masked_prefix'],
-                                 eddy_epi_bse_betmask_prefix=inter['eddy_epi_bse_betmask_prefix'],
-                                 t1_template=args.t1_template,
-                                 t1_align_prefix=inter['t1_align_prefix'],
-                                 t1_mask_prefix=inter['t1_mabsmask_prefix'],
-                                 t2_template=args.t2_template,
-                                 t2_align_prefix=inter['t2_align_prefix'],
-                                 t2_mask_prefix=inter['t2_mabsmask_prefix'],
-                                 fs_dir=inter['fs_dir'],
-                                 mode='witht2',
-                                 fs_in_dwi=inter['fs_in_epi'],
-                                 tract_prefix=inter['eddy_epi_tract_prefix'],
-                                 wmql_out=inter['epi_wmql_dir']))
-
-            elif args.task=='Wmqlqc':
-                jobs.append(Wmqlqc(bids_data_dir=args.bids_data_dir,
-                                   id=id,
-                                   dwi_template=args.dwi_template,
-                                   dwi_align_prefix=inter['dwi_align_prefix'],
-                                   eddy_prefix=inter['eddy_prefix'],
-                                   eddy_epi_prefix=inter['eddy_epi_prefix'],
-                                   eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                   eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                   eddy_epi_bse_masked_prefix=inter['eddy_epi_bse_masked_prefix'],
-                                   eddy_epi_bse_betmask_prefix=inter['eddy_epi_bse_betmask_prefix'],
-                                   t1_template=args.t1_template,
-                                   t1_align_prefix=inter['t1_align_prefix'],
-                                   t1_mask_prefix=inter['t1_mabsmask_prefix'],
-                                   t2_template=args.t2_template,
-                                   t2_align_prefix=inter['t2_align_prefix'],
-                                   t2_mask_prefix=inter['t2_mabsmask_prefix'],
-                                   fs_dir=inter['fs_dir'],
-                                   mode='witht2',
-                                   fs_in_dwi=inter['fs_in_epi'],
-                                   tract_prefix=inter['eddy_epi_tract_prefix'],
-                                   wmql_out=inter['epi_wmql_dir'],
-                                   wmqlqc_out= inter['epi_wmqlqc_dir']))
-
-
-        # just t1_template
-        else:
-            if args.task=='StructMask':
-                jobs.append(StructMask(bids_data_dir=args.bids_data_dir,
-                                       derivatives_dir=derivatives_dir,
-                                       id=id,
-                                       struct_template=args.t1_template))
-
-            elif args.task=='Freesurfer':
-                jobs.append(Freesurfer(bids_data_dir=args.bids_data_dir,
-                                       derivatives_dir=derivatives_dir,
-                                       id=id,
-                                       t1_template=args.t1_template))
-
-            if args.task=='CnnMask':
-                jobs.append(CnnMask(bids_data_dir=args.bids_data_dir,
-                                       derivatives_dir=derivatives_dir,
-                                       id=id,
-                                       dwi_template=args.dwi_template))
-
-            elif args.task=='PnlEddy' or args.task=='FslEddy' or \
-                args.task=='TopupEddy' or args.task=='CnnMaskPnlEddy':
-                jobs.append(eval(args.task)(bids_data_dir=args.bids_data_dir,
-                                    derivatives_dir=derivatives_dir,
-                                    id=id,
-                                    pa_ap_template=args.dwi_template))
-
-
-            elif args.task=='PnlEddyUkf':
-                jobs.append(PnlEddyUkf(bids_data_dir=args.bids_data_dir,
-                                       derivatives_dir=derivatives_dir,
-                                       id=id,
-                                       dwi_template=args.dwi_template))
-
-            elif args.task=='Ukf':
-                jobs.append(Ukf(bids_data_dir = args.bids_data_dir,
-                                id=id,
-                                dwi_template = args.dwi_template,
-                                dwi_align_prefix=inter['dwi_align_prefix'],
-                                eddy_prefix=inter['eddy_prefix'],
-                                eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                tract_prefix= inter['eddy_tract_prefix']))
-
-
-            elif args.task=='Fs2Dwi':
-                jobs.append(Fs2Dwi(bids_data_dir=args.bids_data_dir,
-                                   id=id,
-                                   dwi_template=args.dwi_template,
-                                   dwi_align_prefix=inter['dwi_align_prefix'],
-                                   eddy_prefix=inter['eddy_prefix'],
-                                   eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                   eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                   t1_template=args.t1_template,
-                                   t1_align_prefix=inter['t1_align_prefix'],
-                                   t1_mask_prefix=inter['t1_mabsmask_prefix'],
-                                   fs_dir=inter['fs_dir'],
-                                   fs_in_dwi=inter['fs_in_eddy']))
-
-            elif args.task=='Wmql':
-                jobs.append(Wmql(bids_data_dir=args.bids_data_dir,
-                                 id=id,
-                                 dwi_template=args.dwi_template,
-                                 dwi_align_prefix=inter['dwi_align_prefix'],
-                                 eddy_prefix=inter['eddy_prefix'],
-                                 eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                 eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                 t1_template=args.t1_template,
-                                 t1_align_prefix=inter['t1_align_prefix'],
-                                 t1_mask_prefix=inter['t1_mabsmask_prefix'],
-                                 fs_dir=inter['fs_dir'],
-                                 fs_in_dwi=inter['fs_in_eddy'],
-                                 tract_prefix=inter['eddy_tract_prefix'],
-                                 wmql_out=inter['eddy_wmql_dir']))
-
-            elif args.task=='Wmqlqc':
-                jobs.append(Wmqlqc(bids_data_dir=args.bids_data_dir,
-                                   id=id,
-                                   dwi_template=args.dwi_template,
-                                   dwi_align_prefix=inter['dwi_align_prefix'],
-                                   eddy_prefix=inter['eddy_prefix'],
-                                   eddy_bse_masked_prefix=inter['eddy_bse_masked_prefix'],
-                                   eddy_bse_betmask_prefix=inter['eddy_bse_betmask_prefix'],
-                                   t1_template=args.t1_template,
-                                   t1_align_prefix=inter['t1_align_prefix'],
-                                   t1_mask_prefix=inter['t1_mabsmask_prefix'],
-                                   fs_dir=inter['fs_dir'],
-                                   fs_in_dwi=inter['fs_in_eddy'],
-                                   tract_prefix=inter['eddy_tract_prefix'],
-                                   wmql_out=inter['eddy_wmql_dir'],
-                                   wmqlqc_out= inter['eddy_wmqlqc_dir']))
+                                       t1_align_prefix=inter['t1_align_prefix'],
+                                       t1_mask_prefix=inter['t1_mabsmask_prefix'],
+                                       fs_dir=inter['fs_dir'],
+                                       fs_in_dwi=inter['fs_in_eddy'],
+                                       tract_prefix=inter['eddy_tract_prefix'],
+                                       wmql_out=inter['eddy_wmql_dir'],
+                                       wmqlqc_out= inter['eddy_wmqlqc_dir']))
 
 
 
