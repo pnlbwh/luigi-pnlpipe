18dcf40068a61e35729ea0fba054bf922c939682
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-11-20 23:14:02 -0500

    add json_provenance function

--- workflows/_provenance.py
+++ workflows/_provenance.py
@@ -8,6 +8,7 @@ def write_provenance(obj, output=None):
 
     tree= print_tree(obj)
     history_tree= print_history_tree(obj)
+    json_provenance(obj, output)
       
     with open(pjoin(dirname(__file__), 'provenance.html')) as f:
         template= f.read()
@@ -20,4 +21,25 @@ def write_provenance(obj, output=None):
         f.write(template)
 
 
+
+import json
+
+def _get_provenance(task):
+    
+    prov={}
+    prov['name']=task.task_family
+    prov['params']=task.to_str_params()
+    prov['deps']=[_get_provenance(t) for t in task.deps()]
+
+    return prov
+
+
+def json_provenance(task, output=None):
+    
+    if not output:
+        output=task.output()
+        
+    with open(output.dirname.join(output.stem+'.log.json'),'w') as f:
+        json.dump(_get_provenance(task),f)
+        
 
