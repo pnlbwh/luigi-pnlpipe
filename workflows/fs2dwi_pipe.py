8306d21a01fe3b9da1b6625dcef71818a8d34029
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-12-09 23:30:00 -0500

    stable fs2dwi pipe

--- workflows/fs2dwi_pipe.py
+++ workflows/fs2dwi_pipe.py
@@ -26,23 +26,18 @@ class SelectFsDwiFiles(ExternalTask):
     def output(self):
 
         derivatives_dir= self.bids_data_dir.replace('rawdata', self.derivatives_dir)
-        fsdir = _glob(derivatives_dir, self.fs_template, self.id, self.ses)
 
-        # select the latest dwi
-        dwis = _glob(derivatives_dir, self.dwi_template, self.id, self.ses, multiple=True)
-        dwis.reverse()
-        found=False
+        _, fsdir = _glob(derivatives_dir, self.fs_template, self.id, self.ses)
+
+        _, dwi = _glob(derivatives_dir, self.dwi_template, self.id, self.ses)
+
         for suffix in ['XcUnEdEp_dwi', 'XcUnEd_dwi', 'XcUn_dwi', 'Xc_dwi']:
-
-            for dwi in dwis:
-                if suffix in dwi:
-                    found=True
-                    break
-
-            if found:
+            if suffix in dwi:
                 break
 
 
+        # look for Qc'ed mask first
+        # if not present, return the automatic mask
         bse_mask_dict= {
             'XcUnEdEp_dwi': ['XcUnCNNQcEdEp_mask', 'XcUnCNNEdEp_mask', 'XcUnEdEp_bse'],
             'XcUnEd_dwi': ['XcUnCNNQc_mask', 'XcUnCNN_mask', 'XcUn_bse'],
@@ -50,34 +45,43 @@ class SelectFsDwiFiles(ExternalTask):
             'Xc_dwi': ['XcCNNQc_mask', 'XcCNN_mask', 'Xc_bse']
         }
 
-
+        dwidir= dirname(dwi)
         try:
-            mask= glob(pjoin(dirname(dwi), '*' + bse_mask_dict[suffix][0] + '.nii.gz'))[0]
+            t= pjoin(dwidir, '*' + bse_mask_dict[suffix][0] + '.nii.gz')
+            mask= glob(t)[0]
         except IndexError:
-            mask= glob(pjoin(dirname(dwi), '*' + bse_mask_dict[suffix][1] + '.nii.gz'))[0]
+            t= pjoin(dwidir, '*' + bse_mask_dict[suffix][1] + '.nii.gz')
 
-        bse= glob(pjoin(dirname(dwi), '*'+ bse_mask_dict[suffix][2]+'.nii.gz'))[0]
+            try:
+                mask= glob(t)[0]
+            except:
+                raise FileNotFoundError('Neither *{}.nii.gz nor *{}.nii.gz could be found in {}'
+                                        .format(bse_mask_dict[suffix][0], bse_mask_dict[suffix][1], dwidir))
 
-        print('')
-        print(bse)
-        print(mask)
-        print('')
+        bse= glob(pjoin(dwidir, '*'+ bse_mask_dict[suffix][2]+'.nii.gz'))[0]
 
-        return dict(fsdir=local.path(fsdir), bse=local.path(bse), mask=local.path(mask))
+        return dict(fsdir=local.path(fsdir), dwi=local.path(dwi), bse=local.path(bse), mask=local.path(mask))
 
 
-@requires(SelectFsDwiFiles,StructMask)
+@inherits(SelectFsDwiFiles,StructMask)
 class Fs2Dwi(Task):
 
     debug= BoolParameter(default=False)
     mode= Parameter(default='direct')
+
+    def requires(self):
+        if self.struct_template:
+            return self.clone(SelectFsDwiFiles),self.clone(StructMask)
+        else:
+            return self.clone(SelectFsDwiFiles),
+
 
     def run(self):
         cmd = (' ').join(['fs2dwi.py',
                           '-f', self.input()[0]['fsdir'],
                           '--bse', self.input()[0]['bse'],
                           '--dwimask', self.input()[0]['mask'],
-                          '-o', self.output(),
+                          '-o', self.output()[0].dirname,
                           '-d' if self.debug else '',
                           'direct' if self.mode=='direct'
                           else 'witht2 --t2 {} --t2mask {}'.format(self.input()[1]['aligned'], self.input()[1]['mask'])])
@@ -86,21 +90,24 @@ class Fs2Dwi(Task):
 
     def output(self):
 
-        return local.path(self.input()[1]['bse'].dirname.replace('dwi','fs2dwi'))
+        wmparc= local.path(self.input()[0]['dwi'].dirname.join('wmparcInDwi.nii.gz').replace('dwi', 'fs2dwi'))
+        return wmparc,self.input()[0]['dwi']
 
 
-@requires(Fs2Dwi,Ukf)
+@requires(Fs2Dwi)
 class Wmql(Task):
 
-    # wmql_out= Parameter()
     query= Parameter(default='')
     wmql_nproc= IntParameter(default= int(N_PROC))
 
     def run(self):
+        # obtain the tract from dwi prefix
+        tract= self.input()[1].replace('/dwi/', '/tracts/').replace('_dwi.nii.gz', '.vtk')
+
         cmd = (' ').join(['wmql.py',
                           '-f', self.input()[0],
-                          '-i', self.input()[1],
-                          '-o', self.wmql_out,
+                          '-i', tract,
+                          '-o', self.output(),
                           f'-q {self.query}' if self.query else '',
                           f'-n {self.wmql_nproc}' if self.wmql_nproc else ''])
         p = Popen(cmd, shell=True)
@@ -109,22 +116,24 @@ class Wmql(Task):
 
     def output(self):
 
-        return local.path(self.input()[0].replace('fs2dwi','wmql'))
+        return local.path(self.input()[0].dirname.replace('fs2dwi','wmql'))
 
 
 @requires(Wmql)
 class Wmqlqc(Task):
 
-    # wmqlqc_out= Parameter()
+    id = Parameter()
+    ses = Parameter(default='')
 
     def run(self):
         cmd = (' ').join(['wmqlqc.py',
                           '-i', self.input(),
                           '-s', self.id,
-                          '-o', self.wmqlqc_out])
+                          '-o', self.output()])
         p = Popen(cmd, shell=True)
         p.wait()
 
     def output(self):
-        return local.path(self.input()[0].replace('wmql','wmqlqc'))
+
+        return local.path(self.input().replace('wmql','wmqlqc'))
 
