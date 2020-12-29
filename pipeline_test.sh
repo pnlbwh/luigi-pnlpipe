73df766b125cfcc966bdde8ee4b41be0c0b0e80a
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-12-29 16:55:28 -0500

    all tests put together

--- pipeline_test.sh
+++ pipeline_test.sh
@@ -58,7 +58,11 @@ workflows/ExecuteTask.py --task Ukf --bids-data-dir $HOME/CTE/rawdata -c 1004 -s
 --dwi-template sub-*/ses-01/dwi/*_dwi.nii.gz --t2-template sub-*/ses-01/anat/*_AXT2.nii.gz
 
 # test of EddyEpi (PnlEddy)
-# replace eddy_task in dwi_pipe_params, delete *Ed_dwi.nii.gz
+# replace eddy_task in dwi_pipe_params
+sed -i "s/eddy_task:\ FslEddy/eddy_task:\ PnlEddy/g" test_params/dwi_pipe_params.cfg
+# delete *Ed_dwi.nii.gz
+rm CTE/derivatives/pnlpipe/sub-*/ses-*/dwi/*Ed_dwi.nii.gz
+
 workflows/ExecuteTask.py --task EddyEpi --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
 --dwi-template sub-*/ses-01/dwi/*_dwi.nii.gz --t2-template sub-*/ses-01/anat/*_AXT2.nii.gz
 
@@ -67,18 +71,23 @@ export LUIGI_CONFIG_PATH=`pwd`/test_params/fs2dwi_pipe_params.cfg
 
 # test of Wmql
 # delete *_T2w*nii.gz
+rm CTE/derivatives/pnlpipe/sub-*/ses-*/anat/*_T2w*
 workflows/ExecuteTask.py --task Wmql --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
 --dwi-template sub-*/ses-*/dwi/*EdEp_dwi.nii.gz --t2-template sub-*/ses-*/dwi/*_T2w.nii.gz
 
-
+exit
 
 ### HCP ###
 
 ## dwi pipeline ##
 export LUIGI_CONFIG_PATH=`pwd`/test_params/dwi_pipe_params.cfg
 
-# test of EddyEpi (TopupEddy) and Ukf, delete tracts/
-# replace eddy_epi_task in Ukf, acqp, index
-workflows/ExecuteTask.py --task Ukf --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 1 \
+# test of EddyEpi (TopupEddy) and Ukf
+# replace eddy_epi_task, acqp, index in dwi_pipe_params
+sed -i "s/eddy_epi_task:\ PnlEddy/eddy_task:\ topupeddy/g" test_params/dwi_pipe_params.cfg
+sed -i "s/acqp.txt/acqp_ap_pa.txt/g" test_params/dwi_pipe_params.cfg
+sed -i "s/index.txt//g" test_params/dwi_pipe_params.cfg
+
+workflows/ExecuteTask.py --task Ukf --bids-data-dir $HOME/HCP/rawdata -c 1042 -s 1 \
 --dwi-template sub-*/ses-*/dwi/*acq-PA_dwi.nii.gz,sub-*/ses-*/dwi/*acq-AP_dwi.nii.gz
 
