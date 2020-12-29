a3e577e05459ba4639dec3f56ec4fbf52654953a
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-12-29 15:44:23 -0500

    comprehensive test WIP

--- pipeline_test.sh
+++ pipeline_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -eo pipefail
+
 cd /home/pnlbwh
 export LANG=en_US.UTF-8
 
@@ -33,26 +35,50 @@ fi
 sed -i "356s+cmd+'mv $HOME/rawdata/freesurfer $HOME/derivatives/pnlpipe/sub-1004/ses-01/anat/'+g" luigi-pnlpipe/workflows/struct_pipe.py
 
 
-# provide *_pipe_params
+
 cd luigi-pnlpipe
-export LUIGI_CONFIG_PATH=`pwd`/params/struct_pipe_params.cfg
+
+### CTE ###
 
 
-# define PATH and PYTHONPATH
-export FILTER_METHOD=PYTHON
+## structural pipeline ##
+export LUIGI_CONFIG_PATH=`pwd`/test_params/struct_pipe_params.cfg
+
+workflows/ExecuteTask.py --task StructMask --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
+--t2-template sub-*/ses-01/anat/*_T2w.nii.gz
+
+workflows/ExecuteTask.py --task Freesurfer --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
+--t1-template sub-*/ses-01/anat/*_T1w.nii.gz --t2-template sub-*/ses-01/anat/*_T2w.nii.gz
+
+## dwi pipeline ##
+export LUIGI_CONFIG_PATH=`pwd`/test_params/dwi_pipe_params.cfg
+
+# test of EddyEpi (FslEddy+PnlEpi) and Ukf
+workflows/ExecuteTask.py --task Ukf --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
+--dwi-template sub-*/ses-01/dwi/*_dwi.nii.gz --t2-template sub-*/ses-01/anat/*_AXT2.nii.gz
+
+# test of EddyEpi (PnlEddy)
+# replace eddy_task in dwi_pipe_params, delete *Ed_dwi.nii.gz
+workflows/ExecuteTask.py --task EddyEpi --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
+--dwi-template sub-*/ses-01/dwi/*_dwi.nii.gz --t2-template sub-*/ses-01/anat/*_AXT2.nii.gz
+
+## fs2dwi pipeline ##
+export LUIGI_CONFIG_PATH=`pwd`/test_params/fs2dwi_pipe_params.cfg
+
+# test of Wmql
+# delete *_T2w*nii.gz
+workflows/ExecuteTask.py --task Wmql --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 01 \
+--dwi-template sub-*/ses-*/dwi/*EdEp_dwi.nii.gz --t2-template sub-*/ses-*/dwi/*_T2w.nii.gz
 
 
-# run pipeline
 
-# structural pipeline
-workflows/ExecuteTask.py --task StructMask --bids-data-dir $HOME/rawdata -c 1004 -s 01 --t2-template sub-*/ses-01/anat/*_T2w.nii.gz
+### HCP ###
 
-workflows/ExecuteTask.py --task Freesurfer --bids-data-dir $HOME/rawdata -c 1004 -s 01 --t1-template sub-*/ses-01/anat/*_T1w.nii.gz --t2-template sub-*/ses-01/anat/*_T2w.nii.gz
+## dwi pipeline ##
+export LUIGI_CONFIG_PATH=`pwd`/test_params/dwi_pipe_params.cfg
 
-
-# dwi pipeline
-export LUIGI_CONFIG_PATH=`pwd`/params/dwi_pipe_params.cfg
-
-workflows/ExecuteTask.py --task FslEddyEpi --bids-data-dir $HOME/rawdata -c 1004 -s 01 --dwi-template sub-*/ses-01/dwi/*_dwi.nii.gz --t2-template sub-*/ses-01/anat/*_AXT2.nii.gz
-
+# test of EddyEpi (TopupEddy) and Ukf, delete tracts/
+# replace eddy_epi_task in Ukf, acqp, index
+workflows/ExecuteTask.py --task Ukf --bids-data-dir $HOME/CTE/rawdata -c 1004 -s 1 \
+--dwi-template sub-*/ses-*/dwi/*acq-PA_dwi.nii.gz,sub-*/ses-*/dwi/*acq-AP_dwi.nii.gz
 
