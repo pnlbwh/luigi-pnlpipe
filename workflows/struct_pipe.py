490a21fa11f539d6b7960631bd0a66c2c7f4c561
Billah, Tashrif <tbillah@bwh.harvard.edu>
2021-08-04 17:26:59 -0400

    correct logic for qc_mask to use

--- workflows/struct_pipe.py
+++ workflows/struct_pipe.py
@@ -174,11 +174,13 @@ class N4BiasCorrect(Task):
         # check existence of quality checked mask for MABS only
         # aligned mask won't be quality checked
         if self.csvFile and self.mask_qc:
-            qc_mask= _mask_name(self.input()['mask'], self.mask_qc)
-            if not qc_mask.exists():
-                raise FileNotFoundError(f'{qc_mask} does not exist')
-        
-        cmd = (' ').join(['ImageMath', '3', self.output()['masked'], 'm', self.input()['aligned'], self.input()['mask']])
+            mask_name= _mask_name(self.input()['mask'], self.mask_qc)
+            if not mask_name.exists():
+                raise FileNotFoundError(f'{mask_name} does not exist')
+        else:
+            mask_name= self.input()['mask']
+            
+        cmd = (' ').join(['ImageMath', '3', self.output()['masked'], 'm', self.input()['aligned'], mask_name])
         check_call(cmd, shell=True)
         
         cmd = (' ').join(['N4BiasFieldCorrection', '-d', '3', '-i', self.output()['masked'], '-o', self.output()['n4corr']])
