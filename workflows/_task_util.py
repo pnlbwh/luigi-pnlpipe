69bf0a8cc9ca762679abe32f3f2b976a973a2a2b
Billah, Tashrif <tbillah@bwh.harvard.edu>
2021-08-10 16:17:40 -0400

    separate raise and print blocks

--- workflows/_task_util.py
+++ workflows/_task_util.py
@@ -4,14 +4,18 @@ def _mask_name(mask_name, mask_qc=True):
 
     qc_mask= local.path(mask_name.replace('_mask.nii.gz', 'Qc_mask.nii.gz'))
     
+    msg= """\n
+Quality checked mask not found
+Check the quality of created mask {}
+Once you are done, save the (edited) mask as {}
+\n""".format(mask_name, qc_mask)
+    
     if mask_qc:
         if not qc_mask.exists():
-            raise FileNotFoundError('\n\nQuality checked mask not found\n'
-                  'Check the quality of created mask {}\n'
-                  'Once you are done, save the (edited) mask as {}\n\n'
-                  .format(mask_name, qc_mask))
+            raise FileNotFoundError(msg)
         else:
             return qc_mask
     
     else:
+        print(msg)
         return mask_name
