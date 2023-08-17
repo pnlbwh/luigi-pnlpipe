e54faafc33e4ba40bb45db2baddca248a82d8aa3
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-08-17 14:23:12 -0400

    added a batch parallel execution script that can be a wrapper around many different scripts allowing parallel execution.

---
+++ workflows/batch_parallel_wrapper.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Define the path for the list of cases.
+CASELIST_PATH="/data/pnl/Collaborators/CMA/mtsintou/Emotion/caselist.txt"
+
+# Define the directory where the output logs for each job will be saved.
+OUTPUT_DIR="/data/pnl/Collaborators/CMA/mtsintou/Emotion/Output/"
+
+# Define the path to the script that will be run for each case.
+SCRIPT_PATH="/rfanfs/pnl-zorro/software/pnlpipe3/luigi-pnlpipe/workflows/synb0_eddy.sh"
+
+# Define the maximum number of parallel jobs you want to run at a time.
+MAX_PARALLEL_JOBS=$(nvidia-smi -L | wc -l)
+
+# set the start time for time profiling
+START_TIME=$(date +%s)
+
+# Export CASELIST_PATH so it can be accessed by any scripts or programs we run.
+export CASELIST_PATH
+
+# Count the total number of cases.
+# 'wc -l' counts the number of lines in the file.
+TOTAL_CASES=$(wc -l < "$CASELIST_PATH")
+
+# This function is responsible for running a "batch" of jobs.
+run_batch() {
+    # The start and end indices of the batch are passed as arguments.
+    local start=$1
+    local end=$2
+
+    # Declare an array to keep track of the background job PIDs within this batch.
+    declare -a local_jobs
+
+    # Iterate over each index in the batch range.
+    for (( j=$start; j<=$end; j++ ))
+    do
+        # Set the current index as LSB_JOBINDEX, which may be used by the SCRIPT_PATH.
+        export LSB_JOBINDEX=$j
+        echo "Starting job for LSB_JOBINDEX=$LSB_JOBINDEX"
+
+        # Run the script in the background (& at the end).
+        # Any output (both stdout and stderr) will be redirected to a file named after the current index.
+        "$SCRIPT_PATH" > "${OUTPUT_DIR}/${LSB_JOBINDEX}.txt" 2>&1 &
+
+        # Save the PID (process ID) of the background job we just started in our array.
+        local_jobs+=("$!")
+    done
+
+    # This loop waits for each job in the current batch to complete.
+    # It ensures we don't start a new batch until the current batch is done.
+    for job in "${local_jobs[@]}"
+    do
+        wait $job
+    done
+}
+
+# This loop schedules the jobs in batches.
+for (( i=1; i<=$TOTAL_CASES; i+=MAX_PARALLEL_JOBS ))
+do
+    # Calculate the last index of the current batch.
+    end=$((i + MAX_PARALLEL_JOBS - 1))
+
+    # If the calculated end exceeds the total number of cases, adjust it.
+    if [[ $end -gt $TOTAL_CASES ]]
+    then
+        end=$TOTAL_CASES
+    fi
+
+    # Run the current batch.
+    run_batch $i $end
+done
+
+# Calculate the elapsed time.
+ELAPSED_TIME=$(($(date +%s) - $START_TIME))
+
+# Once all batches have been processed, print a completion message.
+echo "All jobs completed!"
+
+# Print the elapsed time.
+echo "Elapsed time: $(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
--- workflows/mock_synb0_eddy.sh
+++ workflows/mock_synb0_eddy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo "Processing for LSB_JOBINDEX=$LSB_JOBINDEX"
-sleep $((RANDOM % 30 + 10))
+
+# add code to make sure the correct cuda visible device is used, there are 4 gpus so % job index by 4
+export CUDA_VISIBLE_DEVICES=$((LSB_JOBINDEX % 4))
+
+# print out the device id
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+# sleep for a random amount of time between 10 and 30 seconds
+sleep $((RANDOM % 20 + 10))
+
+
 echo "Completed for LSB_JOBINDEX=$LSB_JOBINDEX"
 
